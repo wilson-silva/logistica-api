20df26070f16ea73403af50413710653231714c7
Wilson <wilson-gs@hotmail.com>
2021-08-11 22:07:35 -0300

    implementando p jakarta Persiste

--- src/main/java/com/algaworks/logistica/api/LogisticaApiApplication.java
+++ src/main/java/com/algaworks/logistica/api/LogisticaApiApplication.java
@@ -1,4 +1,4 @@
-package com.algaworks.logisticaapi;
+package com.algaworks.logistica.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
